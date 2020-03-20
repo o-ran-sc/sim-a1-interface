7eb3d229bd527f615eed69535afe5109a8f9da6e
maximesson <maxime.bonneau@est.tech>
2020-03-20 16:20:57 +0100

    Addition to the first commit
    Change-Id: Ib51cb9b558de6493c5f88650b6b508c44cad74bf
    Issue-ID: NONRTRIC-88
    Signed-off-by: maximesson <maxime.bonneau@est.tech>

--- near-rt-ric-simulator/scripts/start_simulator.sh
+++ near-rt-ric-simulator/scripts/start_simulator.sh
@@ -5,9 +5,9 @@
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-#  
+#
 #       http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,9 +18,4 @@
 
 # One argument can be used along with the script call: it is the port on which one wish to run the simulator.
 
-if [ $# -eq 0 ]
-then
-  python3 ./main.py
-else
-  python3 ./main.py $1
-fi
+python3 ./main.py $*
