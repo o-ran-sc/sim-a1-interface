569b7a7d1207286d263ab245e0b3b2eaa3af50a4
halil.cakal <halil.cakal@est.tech>
2022-06-21 12:05:14 +0100

    Kafka dispatcher module backbone
    Patch set 2: Creates a Python-adapter dispatching HTTP/HTTPS requests to a Kafka broker using kafka-python library.
    Patch set 3: Develops a consumer capable of deserializing JSON messages. Clean up redundant functions and parameters.
    Patch set 4: Develops a feature that publishes and consumes (to/from) the Kafka cluster in two-way synch.
    Patch set 5: Develops unit tests cases.
    
    Issue-ID: NONRTRIC-757
    Change-Id: I9bd257691d8a50e81492c55a5ec17a4409d9da6c
    Signed-off-by: halil.cakal <halil.cakal@est.tech>

---
+++ near-rt-ric-simulator/test/KAFKA_DISPATCHER_TEST/basic_test.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+#  ============LICENSE_START===============================================
+#  Copyright (C) 2022 Nordix Foundation. All rights reserved.
+#  ========================================================================
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ============LICENSE_END=================================================
+#
+
+# Script for basic test of the Kafka message dispatcher.
+# Run the build_and_start with the same arg, except arg 'nonsecure|secure', as this script
+
+print_usage() {
+    echo "Usage: ./basic_test.sh nonsecure|secure "
+    exit 1
+}
+
+if [ $# -ne 1 ]; then
+    print_usage
+fi
+if [ "$1" != "nonsecure" ] && [ "$1" != "secure" ]; then
+    print_usage
+fi
+
+if [ $1 == "nonsecure" ]; then
+    #Default http port for the simulator
+    PORT=7075
+    # Set http protocol
+    HTTPX="http"
+else
+    #Default https port for the simulator
+    PORT=7175
+    # Set https protocol
+    HTTPX="https"
+fi
+
+. ../common/test_common.sh
+. ../common/elapse_time_curl.sh
+
+echo "=== Kafka message dispatcher hello world ==="
+RESULT="OK"
+do_curl GET / 200
+
+echo "=== Reset force delay ==="
+RESULT="Force delay has been resetted for all dispatcher responses"
+do_curl POST /dispatcheradmin/forcedelay 200
+
+echo "=== Put an a1 policy: alpha ==="
+RESULT=""
+do_curl PUT  /policytypes/ptype1/kafkadispatcher/alpha 200 jsonfiles/alpha_policy.json
+
+echo "=== Put an a1 policy alpha to update ==="
+RESULT=""
+do_curl PUT  /policytypes/ptype1/kafkadispatcher/alpha 200 jsonfiles/alpha_policy.json
+
+echo "=== Delete an a1 policy: alpha ==="
+RESULT=""
+do_curl DELETE  /policytypes/ptype1/kafkadispatcher/alpha 200
+
+echo "=== Set force delay 5 sec ==="
+RESULT="Force delay: 5 sec set for all dispatcher responses until it is resetted"
+do_curl POST '/dispatcheradmin/forcedelay?delay=5' 200
+
+echo "=== Put an a1 policy alpha to update ==="
+RESULT=""
+do_elapsetime_curl PUT  /policytypes/ptype1/kafkadispatcher/alpha 200 jsonfiles/alpha_policy.json 5
+
+echo "=== Reset force delay ==="
+RESULT="Force delay has been resetted for all dispatcher responses"
+do_curl POST /dispatcheradmin/forcedelay 200
+
+echo "=== Put an a1 policy: beta ==="
+RESULT=""
+do_curl PUT  /policytypes/ptype1/kafkadispatcher/beta 200 jsonfiles/beta_policy.json
+
+echo "=== Put an a1 policy: alpha ==="
+RESULT=""
+do_curl PUT  /policytypes/ptype1/kafkadispatcher/alpha 200 jsonfiles/alpha_policy.json
+
+echo "=== Set force response code: 500 ==="
+RESULT="Force response code: 500 set for all dispatcher response until it is resetted"
+do_curl POST  '/dispatcheradmin/forceresponse?code=500' 200
+
+echo "=== Put an a1 policy: alpha ==="
+res=$(cat jsonfiles/forced_response.json)
+RESULT="json:$res"
+do_curl PUT  /policytypes/ptype1/kafkadispatcher/alpha 500 jsonfiles/alpha_policy.json
+
+echo "=== Reset force response code ==="
+RESULT="Force response code has been resetted for dispatcher responses"
+do_curl POST  /dispatcheradmin/forceresponse 200
+
+echo "=== Delete an a1policy: alpha ==="
+RESULT=""
+do_curl DELETE  /policytypes/ptype1/kafkadispatcher/alpha 200
+
+echo "********************"
+echo "*** All tests ok ***"
+echo "********************"
