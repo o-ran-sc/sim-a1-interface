f94f9086d7dc59d5535a4df5696055a2641bbac0
halil.cakal <halil.cakal@est.tech>
2022-06-21 12:05:14 +0100

    Kafka dispatcher module backbone
    Patch set 2: Creates a Python-adapter dispatching HTTP/HTTPS requests to a Kafka broker using kafka-python library.
    Patch set 3: Develops a consumer capable of deserializing JSON messages. Clean up redundant functions and parameters.
    Patch set 4: Develops a feature that publishes and consumes (to/from) the Kafka cluster in two-way synch.
    Patch set 5: Develops unit tests cases.
    Patch set 6: Develops a feature capable of handling requestid sent in headers for error testing.
    Patch set 7: Develops asynch error test cases.
    Patch set 8: Develops configurable look-up talbe that maps policy-type to kafka-topic. Supports multi topic including unit tests cases with error based.
    
    Issue-ID: NONRTRIC-757
    Change-Id: I9bd257691d8a50e81492c55a5ec17a4409d9da6c
    Signed-off-by: halil.cakal <halil.cakal@est.tech>

---
+++ near-rt-ric-simulator/test/KAFKA_DISPATCHER_TEST/basic_test_with_cust_header.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+#  ============LICENSE_START===============================================
+#  Copyright (C) 2022 Nordix Foundation. All rights reserved.
+#  ========================================================================
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ============LICENSE_END=================================================
+#
+
+# Script for basic test of the Kafka message dispatcher.
+# Run the build_and_start with the same arg, except arg 'nonsecure|secure', as this script
+
+print_usage() {
+    echo "Usage: ./basic_test.sh nonsecure|secure "
+    exit 1
+}
+
+if [ $# -ne 1 ]; then
+    print_usage
+fi
+if [ "$1" != "nonsecure" ] && [ "$1" != "secure" ]; then
+    print_usage
+fi
+
+if [ $1 == "nonsecure" ]; then
+    # Default http port for the simulator
+    PORT=7075
+    # Set http protocol
+    HTTPX="http"
+else
+    #Default https port for the simulator
+    PORT=7175
+    # Set https protocol
+    HTTPX="https"
+fi
+
+. ../common/test_common.sh
+. ../common/elapse_time_curl.sh
+
+echo "=== Kafka message dispatcher hello world ==="
+RESULT="OK"
+do_curl GET / 200
+
+echo "=== Reset force delay ==="
+RESULT="Force delay has been resetted for all dispatcher responses"
+do_curl POST /dispatcheradmin/forcedelay 200
+
+# asynch error test case
+echo "=== Put an a1 policy: shall return time-out ==="
+req_id=$(get_random_number)
+res=$(cat jsonfiles/timeout_response.json)
+RESULT="json:$res"
+# asynch callout
+do_curl PUT  /policytypes/ANR/kafkadispatcher/alpha 408 jsonfiles/alpha_policy.json $req_id &
+proc_id=$!
+sleep 35
+# after 35 seconds, publish the event
+publish_event $req_id
+# wait until the main process to be completed
+wait $proc_id
+
+# asynch success test case after 10s
+echo "=== Put an a1 policy: shall succeed ==="
+req_id=$(get_random_number)
+RESULT=""
+# asynch callout
+do_curl PUT  /policytypes/type2/kafkadispatcher/alpha 200 jsonfiles/alpha_policy.json $req_id &
+proc_id=$!
+sleep 10
+# after 10s, publish the event
+publish_event $req_id
+# wait until the main process to be completed
+wait $proc_id
+
+# asynch success test case without any delay
+echo "=== Delete an a1 policy: shall succeed ==="
+req_id=$(get_random_number)
+RESULT=""
+# asynch callout
+do_curl DELETE  /policytypes/type3/kafkadispatcher/alpha 200 jsonfiles/alpha_policy.json $req_id &
+proc_id=$!
+publish_event $req_id
+# wait until the main process to be completed
+wait $proc_id
+
+
+echo "********************"
+echo "*** All tests ok ***"
+echo "********************"
