5dafb7bc45fba9b100e40f41bb894f833ad11972
halil.cakal <halil.cakal@est.tech>
2022-06-21 12:05:14 +0100

    Kafka dispatcher module backbone
    Patch set 2: Develops Kafka message send logic using kafka-python library.
    Patch set 3: Adds kafka producer capability of sending json messages.
    
    Issue-ID: NONRTRIC-757
    Change-Id: I9bd257691d8a50e81492c55a5ec17a4409d9da6c
    Signed-off-by: halil.cakal <halil.cakal@est.tech>

---
+++ near-rt-ric-simulator/test/KAFKA_DISPATCHER/src/maincommon.py
@@ -0,0 +1,34 @@
+#  ============LICENSE_START===============================================
+#  Copyright (C) 2022 Nordix Foundation. All rights reserved.
+#  ========================================================================
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ============LICENSE_END=================================================
+#
+
+import os
+import sys
+from pathlib import Path
+from flask import Response
+import socket
+import ssl
+
+#Must exist
+apipath=os.environ['APIPATH']
+#May exist
+remote_hosts_logging=os.getenv('REMOTE_HOSTS_LOGGING')
+
+# Make sure the api path for the interface yaml file is set, otherwise exit
+def check_apipath():
+    if (apipath is None):
+        print("Env APIPATH not set. Exiting....")
+        sys.exit(1)
