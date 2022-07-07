806faa34e5f55c1c9e99baa87c4af928252acc75
halil.cakal <halil.cakal@est.tech>
2022-07-07 08:53:12 +0100

    Callout documentation includes readme and docs Patchset 2: Clean-up.
    Issue-ID: NONRTRIC-771
    Change-Id: I65f4ac760d52c8579fdbf2773fa595b60d413c9a
    Signed-off-by: halil.cakal <halil.cakal@est.tech>
    (cherry picked from commit 30f41fcbaa50eef322aa47efe8346b432b96b578)

--- near-rt-ric-simulator/test/EXT_SRV/docs/conf.py
+++ near-rt-ric-simulator/test/EXT_SRV/docs/conf.py
@@ -2,12 +2,12 @@ from docs_conf.conf import *
 
 language = 'en'
 
+extensions = [
+   'sphinxcontrib.openapi',
+]
+
 linkcheck_ignore = [
     'http://localhost.*',
     'http://127.0.0.1.*',
     'https://gerrit.o-ran-sc.org.*'
-]
-
-extensions = [
-   'sphinxcontrib.openapi',
 ]
