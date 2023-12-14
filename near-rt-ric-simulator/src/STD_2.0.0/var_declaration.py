8ccbf459fb38335d3a2ae6b1d92ff7292ef8e478
DenisGNoonan <denis.noonan@est.tech>
2023-12-14 14:18:43 +0000

    NONRTRIC-955: Resolve unit tests 404 error
    Signed-off-by: DenisGNoonan <denis.noonan@est.tech>
    Change-Id: I49eb07adc3aecde7cb1d571160926cac4475d6ca

--- near-rt-ric-simulator/src/STD_2.0.0/var_declaration.py
+++ near-rt-ric-simulator/src/STD_2.0.0/var_declaration.py
@@ -1,5 +1,6 @@
 #  ============LICENSE_START===============================================
 #  Copyright (C) 2021 Nordix Foundation. All rights reserved.
+#  Copyright (C) 2023 OpenInfra Foundation Europe. All rights reserved.
 #  ========================================================================
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -15,18 +16,10 @@
 #  ============LICENSE_END=================================================
 #
 
+import connexion
 from maincommon import apipath
-from flask import Flask
-from connexion import FlaskApp
 
-flask_app = Flask(__name__)
-
-# Main app
-app = FlaskApp(__name__, specification_dir=apipath)
-app.add_api('ORAN_A1-p_V2.0.0_api.yaml')
-
-# Combine Connexion app with Flask app
-app.app = flask_app
+app = connexion.App(__name__, specification_dir=apipath)
 
 policy_types={}
 policy_instances = {}
